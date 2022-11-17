8e7dea0e6e9da3f848bbb7b95986924c77479797
marinkruis <marinkruis@ziggo.nl>
2022-11-17 11:41:19 +0100

    options
    ik heb de options toegevoegd

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -9,28 +9,35 @@ def cmd_group():
 
 @cmd_group.command("sin")
 @click.argument("number",type=int)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="number of steps from 0 to 2pi",
+    show_default=True, #show default in help
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-@click.command("tan")
-@click.argument("number")
+@cmd_group.command("tan")
+@click.argument("number", type=int)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="number of steps from 0 to 2pi",
+    show_default=True, #show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
 if __name__ == "__main__":
     cmd_group()
 
-#     @click.option(
-#     "-n",
-#     "--number",
-#     default=10,
-#     help="number of steps from 0 to 2pi",
-#     show_default=True, #show default in help
-# )
+

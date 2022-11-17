4ac13b9b6df839e15395442833ace86a265e37ab
marinkruis <marinkruis@ziggo.nl>
2022-11-17 10:33:27 +0100

    code voor stapjes tussen 0 en 2pi
    ik heb hier de command aangemaakt die de gebruiker de stapjes laat bepalen tussen 0 en 2pi, als default gebruikt hij 10 stapjes

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,7 +3,15 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-
+@click.command()
+@click.argument("number")
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="number of steps from 0 to 2pi",
+    show_default=True, #show default in help
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})

ca8ecd9090e4643433572188fbc82d912c3b88be
marinkruis <marinkruis@ziggo.nl>
2022-11-17 11:37:54 +0100

    group aangemaakt
    ik heb de group aangemaakt maar de code herkende de commands niet. Dat kwam omdat ik click.command gebruikte in plaats van cmd_group.command. Joris heeft me hier bij geholpen.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,22 +3,20 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.command()
-@click.argument("number")
-@click.option(
-    "-n",
-    "--number",
-    default=10,
-    help="number of steps from 0 to 2pi",
-    show_default=True, #show default in help
-)
+@click.group()
+def cmd_group():
+    pass
+
+@cmd_group.command("sin")
+@click.argument("number",type=int)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@click.command("tan")
+@click.argument("number")
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -27,4 +25,12 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+
+#     @click.option(
+#     "-n",
+#     "--number",
+#     default=10,
+#     help="number of steps from 0 to 2pi",
+#     show_default=True, #show default in help
+# )

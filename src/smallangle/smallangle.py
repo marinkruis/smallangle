58ffce9194111f5234963671b0a5ce7cb47e9241
marinkruis <marinkruis@ziggo.nl>
2022-11-17 11:49:29 +0100

    comments

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,11 +3,14 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+# creates group
 @click.group()
 def cmd_group():
     pass
 
+# creates the sin command
 @cmd_group.command()
+# lets user pick how many steps it takes from 0 to 2pi with 10 as default
 @click.option(
     "-n",
     "--number",
@@ -15,12 +18,14 @@ def cmd_group():
     help="number of steps from 0 to 2pi",
     show_default=True, #show default in help
 )
+# function that plots the results
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
+# code is an analogy of above
 @cmd_group.command()
 @click.option(
     "-n",
